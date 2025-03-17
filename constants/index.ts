2d2d50e04d2820c7a8b93ad9357bb3a087ecef75
Rahul Mijar <rahulmijar0243@gmail.com>
2025-03-17 20:00:55 +0545

    feat: add feature's cards

---
+++ constants/index.ts
@@ -0,0 +1,68 @@
+export const featureList = [
+  {
+    image: "https://magnuslens.com/images/faeture1.png",
+    title: "Ultra Thin",
+    text: "Lens",
+    color: "white",
+  },
+  {
+    image: "https://magnuslens.com/images/faeture2.png",
+    title: "Zero Distortion",
+    text: "Lens",
+    color: "gray-100",
+  },
+  {
+    image: "https://magnuslens.com/images/faeture3.png",
+    title: "Relieves Eye",
+    text: "Strain",
+    color: "white",
+  },
+  {
+    image: "https://magnuslens.com/images/faeture4.png",
+    title: "Clear Base",
+    text: "Color",
+    color: "gray-100",
+  },
+  {
+    image: "https://magnuslens.com/images/faeture5.png",
+    title: "Night Drive",
+    text: "Lens",
+    color: "white",
+  },
+  {
+    image: "https://magnuslens.com/images/faeture6.png",
+    title: "Dust & Smudge",
+    text: "Proof",
+    color: "gray-100",
+  },
+  {
+    image: "https://magnuslens.com/images/faeture7.png",
+    title: "Super Hydrophobic",
+    text: "Coating",
+    color: "white",
+  },
+  {
+    image: "https://magnuslens.com/images/faeture8.png",
+    title: "Aspheric",
+    text: "Design",
+    color: "gray-100",
+  },
+  {
+    image: "https://magnuslens.com/images/faeture9.png",
+    title: "Scratch",
+    text: "Resistant",
+    color: "white",
+  },
+  {
+    image: "https://magnuslens.com/images/faeture10.png",
+    title: "Front + Back UV",
+    text: "Protection",
+    color: "gray-100",
+  },
+  {
+    image: "https://magnuslens.com/images/faeture11.png",
+    title: "Digital Screen",
+    text: "Protection",
+    color: "white",
+  },
+];
