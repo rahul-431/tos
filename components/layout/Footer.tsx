06286fc5a22e3d164d6accb360cf997b22877181
Rahul Mijar <rahulmijar0243@gmail.com>
2025-03-17 17:49:39 +0545

    feat: add nav and footer

---
+++ components/layout/Footer.tsx
@@ -0,0 +1,81 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import { FaFacebookF, FaInstagram, FaLocationDot } from "react-icons/fa6";
+import { IoMail } from "react-icons/io5";
+
+const Footer = () => {
+  return (
+    <div className="w-full footerBg px-4 sm:px-8 md:px-12 lg:px-20 py-12 flex sm:flex-row flex-col justify-between gap-10">
+      <div className="flex flex-col items-start">
+        <div className="w-36 h-32">
+          <Image
+            src="/logo1.png"
+            alt="logo"
+            height={100}
+            width={100}
+            className="object-contain w-full h-full rounded-b-2xl"
+            priority
+          />
+        </div>
+        <div className="flex flex-col gap-3">
+          <h1 className="font-bold text-3xl font-sans tracking-wider">
+            Lens Club Pvt. Ltd.
+          </h1>
+          <p className="flex items-center space-x-2 text-lg font-medium">
+            <span className="text-2xl">
+              <IoMail />
+            </span>
+            <span>sales@tos.com</span>
+          </p>
+          <p className="flex flex-col text-lg">
+            <span className="font-semibold">Corporate Office</span>
+            <span className="font-medium flex space-x-2 items-center">
+              <span>
+                <FaLocationDot />
+              </span>
+              <span>khasibazar, kathmandu-2, nepal</span>
+            </span>
+          </p>
+          <p className="flex flex-col font-semibold text-lg">
+            <span className="font-semibold">Sales Office</span>
+            <span className="font-medium flex space-x-2 items-center">
+              <span>
+                <FaLocationDot />
+              </span>
+              <span>Kumarigalli-2, kathmandu, nepal</span>
+            </span>
+          </p>
+        </div>
+      </div>
+      <div className="flex flex-col gap-4 sm:items-end">
+        <div className="md:w-[250px] md:h-[250px] w-full h-80">
+          <Image
+            src="/qr.png"
+            alt="Scan Me"
+            height={0}
+            width={0}
+            sizes="100vw"
+            className="w-full h-full object-cover"
+          />
+        </div>
+        <div className="flex gap-5 items-center">
+          <Link
+            href={"#"}
+            className="text-2xl p-2 sm:p-4 rounded-full border-white border hover:bg-white hover:text-[#03305A]"
+          >
+            <FaFacebookF />
+          </Link>
+          <Link
+            href={"#"}
+            className="text-2xl p-2 sm:p-4 rounded-full border-white border hover:bg-white hover:text-[#03305A]"
+          >
+            <FaInstagram />
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ components/layout/Navbar.tsx
@@ -0,0 +1,92 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import { FaBarsStaggered } from "react-icons/fa6";
+
+const Navbar = () => {
+  const navList = [
+    {
+      label: "About Us",
+      href: "#about",
+    },
+    {
+      label: "Features",
+      href: "#features",
+    },
+    {
+      label: "Products",
+      href: "#products",
+    },
+    {
+      label: "Connect",
+      href: "#connect",
+    },
+  ];
+  return (
+    <div className="flex justify-between items-center px-4 sm:px-8 md:px-12 lg:px-20 bg-white">
+      <Link className="w-16 h-16 sm:w-20 sm:h-20 " href="/">
+        <Image
+          src="/logo.png"
+          alt=""
+          height={100}
+          width={100}
+          className="object-contain w-full h-full rounded-b-2xl"
+          priority
+        />
+      </Link>
+
+      <div className="flex gap-5 text-black items-center">
+        <div className="md:flex hidden gap-8 items-center">
+          {navList.map((item, index) => (
+            <Link
+              href={item.href}
+              key={index}
+              className="text-[#03305A]  hover:text-[#07447D] font-semibold"
+            >
+              {item.label}
+            </Link>
+          ))}
+        </div>
+
+        <Link
+          href="/"
+          className="text-black font-semibold py-1.5 md:py-2 px-4 sm:px-8 rounded-full digital"
+        >
+          Digital Brochure
+        </Link>
+        <Sheet>
+          <SheetTrigger className="cursor-pointer md:hidden text-xl">
+            <FaBarsStaggered />
+          </SheetTrigger>
+          <SheetContent className="max-w-44 bg-[#03305A]">
+            <SheetHeader>
+              <SheetTitle className="text-white">
+                Total Optical Solution
+              </SheetTitle>
+            </SheetHeader>
+            <div className="flex flex-col gap-2 py-2 px-4">
+              {navList.map((item, index) => (
+                <Link
+                  href={item.href}
+                  key={index}
+                  className="text-white font-semibold"
+                >
+                  {item.label}
+                </Link>
+              ))}
+            </div>
+          </SheetContent>
+        </Sheet>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
