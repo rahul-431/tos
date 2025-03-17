2fb266dd0d48a7c3db6fd7de330ce295c8312800
Rahul Mijar <rahulmijar0243@gmail.com>
2025-03-17 19:08:46 +0545

    add about section and fixed footer and nav

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import { FaTiktok, FaWhatsapp } from "react-icons/fa";
 import { FaFacebookF, FaInstagram, FaLocationDot } from "react-icons/fa6";
+import { IoLogoWhatsapp } from "react-icons/io";
 import { IoMail } from "react-icons/io5";
 
 const Footer = () => {
   return (
-    <div className="w-full footerBg px-4 sm:px-8 md:px-12 lg:px-20 py-12 flex sm:flex-row flex-col justify-between gap-10 text-white">
+    <div className="w-full footerBg px-4 sm:px-8 md:px-12 lg:px-20 py-12 flex sm:flex-row flex-col justify-between items-center gap-10 text-white">
       <div className="flex flex-col items-start">
         <div className="w-36 h-32">
           <Image
@@ -48,7 +50,7 @@ const Footer = () => {
           </p>
         </div>
       </div>
-      <div className="flex flex-col gap-4 sm:items-end">
+      <div className="flex flex-col gap-6 items-center">
         <div className="md:w-[250px] md:h-[250px] w-full h-80">
           <Image
             src="/qr.png"
@@ -62,13 +64,25 @@ const Footer = () => {
         <div className="flex gap-5 items-center">
           <Link
             href={"#"}
-            className="text-2xl p-2 sm:p-4 rounded-full border-white border hover:bg-white hover:text-[#03305A]"
+            className="text-2xl p-2 rounded-full border-white border hover:bg-white hover:text-[#03305A]"
+          >
+            <FaTiktok />
+          </Link>
+          <Link
+            href={"#"}
+            className="text-2xl p-2 rounded-full border-white border hover:bg-white hover:text-[#03305A]"
+          >
+            <IoLogoWhatsapp />
+          </Link>
+          <Link
+            href={"#"}
+            className="text-2xl p-2 rounded-full border-white border hover:bg-white hover:text-[#03305A]"
           >
             <FaFacebookF />
           </Link>
           <Link
             href={"#"}
-            className="text-2xl p-2 sm:p-4 rounded-full border-white border hover:bg-white hover:text-[#03305A]"
+            className="text-2xl p-2 rounded-full border-white border hover:bg-white hover:text-[#03305A]"
           >
             <FaInstagram />
           </Link>
---
+++ components/others/About.tsx
@@ -0,0 +1,48 @@
+import Image from "next/image";
+import React from "react";
+
+const About = () => {
+  return (
+    <div className="grid grid-cols-1 sm:grid-cols-2 gap-8 sm:gap-14 lg:gap-20  px-4 sm:px-8 md:px-12 lg:px-20 py-8">
+      {/* image section */}
+      <div className="w-full">
+        <Image
+          src="https://magnuslens.com/images/why-mags1.png"
+          alt="image"
+          width={0}
+          height={0}
+          sizes="100vw"
+          className="w-full h-full object-contain"
+        />
+      </div>
+
+      {/* why choose us section */}
+      <div className="flex flex-col gap-6 ">
+        <div className="flex flex-col gap-2">
+          <h2 className="text-xl sm:text-2xl lg:text-3xl font-bold">
+            Why Choose
+          </h2>
+          <h1 className="text-3xl sm:text-5xl lg:text-6xl font-extrabold textBg w-fit">
+            Magnus Lens?
+          </h1>
+        </div>
+        <div className="flex flex-col gap-8 text-base sm:text-lg font-medium">
+          <p className="text-justify">
+            Magnus Lenses have been specially designed to cater to the needs of
+            Indian users. These lenses have in-built blue filters which reduces
+            digital eye fatigue from prolonged exposure to digital screens.
+          </p>
+          <p className="text-justify">
+            They have an aspheric design and 75mm dia which makes them upto 40%
+            thinner and flatter as compared to normal lenses. Also, the lenses
+            have super hydrophobic coating i.e. dust and smudge proof. These
+            lenses provide protection from harmful UV Rays and have a clear base
+            colour.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
