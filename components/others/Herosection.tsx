eab1be48333f8c38d663b912b4f8e6be8aa386a5
Rahul Mijar <rahulmijar0243@gmail.com>
2025-03-18 13:32:33 +0545

    fix: herosection

--- components/others/Herosection.tsx
+++ components/others/Herosection.tsx
@@ -3,23 +3,35 @@ import * as React from "react";
 import Autoplay from "embla-carousel-autoplay";
 import {
   Carousel,
+  type CarouselApi,
   CarouselContent,
   CarouselItem,
-  CarouselNext,
-  CarouselPrevious,
 } from "@/components/ui/carousel";
 import { HeroData } from "@/constants";
 
 export function HeroSection() {
-  const plugin = React.useRef(
-    Autoplay({ delay: 2500, stopOnInteraction: true })
-  );
+  const [api, setApi] = React.useState<CarouselApi>();
+  const [current, setCurrent] = React.useState(0);
+  const [count, setCount] = React.useState<number>(0);
+
+  React.useEffect(() => {
+    if (!api) {
+      return;
+    }
+
+    setCount(api.scrollSnapList().length);
+    setCurrent(api.selectedScrollSnap() + 1);
+
+    api.on("select", () => {
+      setCurrent(api.selectedScrollSnap() + 1);
+    });
+  }, [api]);
+  const plugin = React.useRef(Autoplay({ delay: 4000, jump: true }));
   return (
     <Carousel
+      setApi={setApi}
       plugins={[plugin.current]}
       className="w-full relative mb-4 sm:mb-0"
-      onMouseEnter={plugin.current.stop}
-      onMouseLeave={plugin.current.reset}
     >
       <CarouselContent>
         {HeroData.map((item, index) => (
@@ -28,12 +40,23 @@ export function HeroSection() {
             style={{
               backgroundImage: `url('${item.image}')`,
             }}
-            className="relative h-[180px] sm:h-[350px] md:h-[700px] w-full bg-center bg-cover bg-no-repeat"
+            className=" h-[180px] sm:h-[350px] md:h-[700px] w-full bg-center bg-cover bg-no-repeat"
           ></CarouselItem>
         ))}
       </CarouselContent>
-      <CarouselPrevious className="absolute left-5 sm:left-12 md:left-20 lg:left-28 h-7 w-7 sm:h-10 sm:w-10 md:h-12 md:w-12 border border-white bg-transparent hover:bg-white hover:text-black text-white" />
-      <CarouselNext className="absolute right-5 sm:right-12 md:right-20 lg:right-28 h-7 w-7 sm:h-10 sm:w-10 md:h-12 md:w-12 border border-white bg-transparent hover:bg-white hover:text-black text-white" />
+      <div className="absolute text-red-600 flex gap-2 bottom-5 left-[35%] sm:left-[42.5%]">
+        {Array.from({ length: count }).map((_, index) => {
+          console.log(index, current);
+          return (
+            <div
+              key={index}
+              className={`${
+                index + 1 === current ? "footerBg" : "bg-white"
+              } h-1 w-4 md:h-2 md:w-10 rounded-md md:rounded-2xl`}
+            />
+          );
+        })}
+      </div>
     </Carousel>
   );
 }
